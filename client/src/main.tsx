73a5d313a0720d06f3c70707c36fe2d52f7df33b
Basil Fernandaz <speak2basilf@gmail.com>
2025-07-24 02:44:47 +0530

    rate

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+} else {
+  console.error("Root element not found. Make sure index.html has <div id='root'></div>");
+}
